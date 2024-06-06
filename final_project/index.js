19b865be06ed754fb7c136335dae74fe1e466a81
deekshithvarma256 <108060741+deekshithvarma256@users.noreply.github.com>
2024-06-07 04:09:37 +0530

    Update index.js

--- final_project/index.js
+++ final_project/index.js
@@ -11,7 +11,19 @@ app.use(express.json());
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+//Write the authentication mechanism here
+    const token = req.session.token;
+    if(token) {
+    jwt.verify(token, 'jwt_secret_key', (err, user) => {
+            if(err){
+                return res.status(401).json({ message: "Unauthorized access." });
+            }
+            req.user = user;
+            next();
+        })
+    } else {
+        return res.status(403).json({ message: 'JWT token required.'})
+    }
 });
  
 const PORT =5000;
